60c86aaef9f40afdba7391c967894c0374f237bd
Oscar Arias <ariasp26@gmail.com>
2024-07-09 22:36:29 -0500

    changes

--- front/src/api/config.ts
+++ front/src/api/config.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const HOST : string | undefined = process.env.REACT_APP_API_URL
+const HOST : string | undefined = "http://ec2-54-87-170-124.compute-1.amazonaws.com"
 console.log("el hsot es ", HOST)
 const apiClient = axios.create({
   baseURL: HOST, 
